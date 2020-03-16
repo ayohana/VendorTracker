1dd18eecd5fd3cdd6b614b8268b2f4adab33a801
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-16 15:34:54 -0700

    Overload order constructor, refactor GetAll() to return all records from database

--- VendorTracker.Solution/VendorTracker/Models/Order.cs
+++ VendorTracker.Solution/VendorTracker/Models/Order.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace VendorTracker.Models
 {
@@ -11,7 +12,6 @@ namespace VendorTracker.Models
     public int Price { get; set; }
     public DateTime DeliveryDate { get; set; }
     public int ID { get; }
-    private static List<Order> _allOrders = new List<Order> { };
 
     public Order(string title, string description, int quantity, int price, DateTime deliveryDate)
     {
@@ -20,23 +20,53 @@ namespace VendorTracker.Models
       Quantity = quantity;
       Price = price;
       DeliveryDate = deliveryDate;
-      _allOrders.Add(this);
-      ID = _allOrders.Count;
+    }
+
+    public Order(int id, string title, string description, int quantity, int price, DateTime deliveryDate)
+    {
+      ID = id;
+      Title = title;
+      Description = description;
+      Quantity = quantity;
+      Price = price;
+      DeliveryDate = deliveryDate;
     }
 
     public static void ClearAll()
     {
-      _allOrders.Clear();
+      // add code connected to database here
     }
 
     public static List<Order> GetAll()
     {
-      return _allOrders;
+      List<Order> allOrders = new List<Order> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM orders;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int orderID = rdr.GetInt32(0);
+        string orderTitle = rdr.GetString(1);
+        string orderDesc = rdr.GetString(2);
+        int orderQuantity = rdr.GetInt32(3);
+        int orderPrice = rdr.GetInt32(4);
+        DateTime orderDeliveryDate = rdr.GetDateTime(5);
+        Order newOrder = new Order(orderID, orderTitle, orderDesc, orderQuantity, orderPrice, orderDeliveryDate);
+        allOrders.Add(newOrder);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allOrders;
     }
 
     public static Order Find(int searchID)
     {
-      return _allOrders[searchID-1];
+      return new Order("placeholder title", "placeholder desc", 1, 1, new DateTime(2020, 4, 1));
     }
     
   }
